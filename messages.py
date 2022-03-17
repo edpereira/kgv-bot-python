69f1c04680d2d7e39888a86a6095455b1e338a5f
Eduardo <eduardo.pereira.ext@panonline.com.br>
2022-03-16 23:13:49 -0300

    Initial commit

---
+++ messages.py
@@ -0,0 +1,7 @@
+CALENDAR_CALLBACK = "CALENDAR"
+
+start_message = "Hey {}! I am calender bot \n \n Please type /calendar or /jcalendar to view my power"
+
+calendar_message = "Please select a date to wacting for avaliable spots: "
+
+calendar_response_message = "/add_%s"
